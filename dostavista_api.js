8e315c27cc3a2dfe88160f38fe4915d19ba434a5
Oleg Gromov <mail@oleggromov.com>
2014-02-13 22:41:01 +0400

    * Добавляет callback-и, обработку JSONP-ошибок на основе таймаута, * Переименовывает классы, * Переименовывает параметры (как в серверном API), * Обновляет README (не до конца)

--- dostavista_api.js
+++ dostavista_api.js
@@ -9,15 +9,23 @@
  * 
  * @author Oleg Gromov <mail@oleggromov.com>
  */
-(function DSTA(global, document, $) {
+(function Dostavista(global, document, $) {
 	/**
 	 * Отключает ВСЕ сообщения плагина.
 	 * @type {Boolean}
 	 */
 	var noDebug = false;
 
+	var jsonpTimeout = 2 * 1000;
+
 	// var apiUrl = 'http://dostavista.ru/bapi/order';
 	var apiUrl = 'http://beta.dostavista.ru/bapi/order';
+
+	var callbacks = {
+		onParamsIllegal: null,
+		onSendSuccess: null,
+		onSendError: null
+	};
 
 	var authParams = {};
 
@@ -57,60 +65,116 @@
 		authParams.token = params.token || false;
 	};
 
+
+	/**
+	 * Добавляет колбэк типа type, который берётся из ключей хэша callbacks.
+	 * 
+	 * @param  {Strong}   type Строка-тип колбека
+	 * @param  {Function} fn   Колбэк
+	 * @return {Boolean}       True, если колбэк сохранён
+	 */
+	var setCallback = function(type, fn) {
+		if (!type || !fn) {
+			_error('Недостаточно параметров.');
+			return false;
+		}
+
+		if (!callbacks.hasOwnProperty(type)) {
+			_error('Недопустимый тип колбэка.');
+			return false;
+		}
+
+		if (typeof fn !== 'function') {
+			_error('Второй аргумент должен быть функцией.');
+			return false;
+		}
+
+		callbacks[type] = fn;
+		return true;
+	};
+
+
 	/**
 	 * Обрабатывает клик по кнопке, проверяет параметры и отправляет их на сервер.
 	 * 
 	 * @param  {Object} e Объект-событие.
 	 */
 	var handleClick = function(e) {
-		e.preventDefault();
+		var button = this;
+
+		if (!_canSend.call(button)) return;
+
+		_setButtonState.call(button, 'sending');
 
 		if (!authParams.client_id || !authParams.token) {
 			_error('Установите clientId и token сразу после подключения плагина.');
 			return;
 		}
 
-		var params = _parseParams.call(this);
+		var params = _parseParams.call(button);
 		try {
 			_checkParams(params);
 		} catch (e) {
-			_error(e);
-			// TODO хук для отсутствующих параметров
-			return;
+			if (typeof callbacks['onParamsIllegal'] === 'function') {
+				callbacks['onParamsIllegal'](params, e);
+			} else {
+				_error(e);
+				_setButtonState.call(button, 'error', e);
+
+				return;
+			}
 		}
 
 		var apiCall = _sendOrder(params);
-		apiCall.done(function ajaxDone(result) {
-			console.log(result);
+		apiCall.done(function ajaxDone(resJSON) {
+			if (typeof callbacks['onSendSuccess'] === 'function') {
+				callbacks['onSendSuccess'](resJSON);
+			}
 
-			// TODO хук для обработки успешного результата
+			_setButtonState.call(button, 'sent', 'ID заказа в Достависте: ' + resJSON.order_id);
 		});
 
 		apiCall.fail(function ajaxFail(jqxhr, text, error) {
-			console.log(text);
-			console.log(error);
+			if (typeof callbacks['onSendError'] === 'function') {
+				callbacks['onSendError'](text, error);
+			} else {
+				_error(text);
+			}
 
-			// хук для обработки ошибки.
+			_setButtonState.call(button, 'error', text);
 		});
 	};
 
 
 	/**
 	 * Делает AJAX-запрос к API Достависты, получая результат в JSONP.
+	 * Использует свой Deferred для обработки ошибок, чтобы делать promise.reject() при таймауте.
 	 * 
 	 * @param  {Object} params Хэш с обработанными параметрами
 	 * @return {Promise} Разрешается, когда приходит ответ от сервера.
 	 */
 	var _sendOrder = function(params) {
 		params = $.extend(params, authParams);
-
-		return $.ajax({
+		
+		var def = $.Deferred();
+		$.ajax({
 			data: params,
-			url: apiUrl,
+			// url: apiUrl,
+			url: 'http://localhost',
 			type: 'post',
 			dataType: 'jsonp',
 			cache: false
+		}).done(function(result) {
+			def.resolve(result);
+		}).fail(function(jqxhr, text, error) {
+			def.reject(jqxhr, text, error);
 		});
+
+		setTimeout(function() {
+			def.reject(null, 'Ответа нет слишком долго. Возможно, проблемы с сетью.', null);
+		}, jsonpTimeout);
+
+		return def.promise();
 	};
 
 
@@ -180,11 +244,11 @@
 		var error = "";
 
 		if (!params.matter) error += 'Не задан параметр matter.\n';
-		if (!params.point[0]['address']) error += 'Не задан параметр address.\n';
-		if (!params.point[0]['phone']) error += 'Не задан параметр phone.\n';
-		if (!params.point[0]['required_time_start']) error += 'Не задан параметр time_start.\n';
-		if (!params.point[0]['required_time']) error += 'Не задан параметр time.\n';
-		if (!params.point[0]['weight']) error += 'Не задан параметр weight.\n';
+		if (!params.point[0]['address']) error += 'Не задан параметр point[0].address.\n';
+		if (!params.point[0]['phone']) error += 'Не задан параметр point[0].phone.\n';
+		if (!params.point[0]['required_time_start']) error += 'Не задан параметр point[0].time_start.\n';
+		if (!params.point[0]['required_time']) error += 'Не задан параметр point[0].time.\n';
+		if (!params.point[0]['weight']) error += 'Не задан параметр point[0].weight.\n';
 
 		if (error) {
 			throw error;
@@ -192,11 +256,40 @@
 	}
 
 
+	/**
+	 * Устанавливает состояние кнопки — CSS-класс, disabled и title.
+	 * 
+	 * @param {String} state Одно из ['sending', 'sent', 'error']
+	 */
+	var _setButtonState = function(state, title) {
+		$(this).removeClass();
+		$(this).addClass('DostavistaButton');
+		$(this).addClass('DostavistaButton_' + state);
+
+		if ($.inArray(state, ['sending', 'sent'])) {
+			$(this).addClass('DostavistaButton_disabled');
+		}
+
+		$(this).removeAttr('title');
+		if (title) $(this).attr('title', title);
+	};
+
+	/**
+	 * Определяет, можно ли нажимать на кнопку.
+	 * 
+	 * @return {Boolean}
+	 */
+	var _canSend = function() {
+		return !$(this).hasClass('DostavistaButton_disabled');
+	};
+
+
 	// Обработчики событий
-	$(document).on('click', '.DSTA_button', handleClick);
+	$(document).on('click', '.DostavistaButton', handleClick);
 
 	// Глобально доступный интерфейс.
 	global.DostavistaApi = {
-		setClient: setClient
+		setClient: setClient,
+		setCallback: setCallback
 	};
 })(window, document, jQuery);
